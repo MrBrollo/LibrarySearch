fadab1c85611cb4a57070e060119665fd3e9bad3
MrBrollo <matteo.ricci0304@gmail.com>
2024-08-12 18:22:40 +0200

    compress plugin

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -12,7 +13,6 @@ module.exports = {
     module: {
         rules: [
             loaders, [{
-                loader: 'babel',
                 exclude: /node_modules/
             }]
         ]
